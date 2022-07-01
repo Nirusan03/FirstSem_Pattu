379511d14e30fd28dfc3c13d2f7c1fde75fdbde7
Nirusan_Hariharan <hariharan.20200094@iit.ac.lk>
2022-07-01 21:11:35 +0530

    View and Initialize

--- src/Task1_Array.java
+++ src/Task1_Array.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Task1_Array {
+    public static int fuelStock = 6600;
+    public static Scanner UserInput = new Scanner(System.in);
     public static void main(String[] args) {
-        int fuelStock = 6600;
         String[][] customer_queue = new String[3][6];
         String customer;
         initialise(customer_queue);
@@ -20,5 +21,22 @@ public class Task1_Array {
                 customer_queue[i][j] = "e";
             }
         }
+    }
+
+    public static void viewQueue(String[][] customer_queue){
+        System.out.println("Gasoline Pump 1  |  Gasoline Pump 2  |  Gasoline Pump 3  ");
+        for(int i = 0; i < customer_queue.length; i++){
+            for (int j = 0; j < customer_queue[i].length; j++){
+                if (customer_queue[i][j].equals("e"))
+                    System.out.print("Free   ");
+                else
+                    System.out.print(customer_queue[i][j]);
+            }
+        }
+    }
+
+    public static void addCustomer(String[][] customer_queue){
+        System.out.print("Enter customer's name : ");
+        String name  = UserInput.nextLine();
     }
 }
